1658e2ed7a6e327a15cfad809414eb3e4562390f
KSKIM <kwangsoonkim@outlook.com>
2019-12-19 14:45:45 +0900

    feat: Number가 볼과 스트라이크를 계산하는 기능 추가

--- src/main/java/com/woowacourse/baseball/domain/number/Number.java
+++ src/main/java/com/woowacourse/baseball/domain/number/Number.java
@@ -6,6 +6,11 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import com.woowacourse.baseball.domain.ballcount.Ball;
+import com.woowacourse.baseball.domain.ballcount.BallCount;
+import com.woowacourse.baseball.domain.ballcount.Strike;
 
 /**
  * @version 1.0.0
@@ -15,7 +20,6 @@ import java.util.stream.Collectors;
 public class Number {
 	private static final String SIZE_EXCEPTION = "세 자리 숫자로 구성되어 있지 않습니다.";
 	private static final String DUPLICATE_EXCEPTION = "숫자간에 중복이 포함되어 있습니다.";
-
 	public static final int TOTAL_NUMBER_COUNT = 3;
 
 	private final List<Digit> number;
@@ -49,5 +53,38 @@ public class Number {
 				.mapToObj(Digit::of)
 				.collect(Collectors.toList());
 		return new Number(Collections.unmodifiableList(number));
+	}
+
+	public BallCount getBallCount(Number target) {
+		Strike strike = getStrike(target);
+		Ball ball = getBall(target);
+		return new BallCount(strike, ball);
+	}
+
+	private Strike getStrike(Number target) {
+		int count = (int) IntStream.range(0, number.size())
+				.filter(index -> target.equalsAt(index, number.get(index)))
+				.count();
+		return new Strike(count);
+	}
+
+	private Ball getBall(Number target) {
+		int count = (int) IntStream.range(0, number.size())
+				.filter(index -> isBall(index, target))
+				.count();
+		return new Ball(count);
+	}
+
+	private boolean isBall(int index, Number target) {
+		Digit digit = number.get(index);
+		return !target.equalsAt(index, digit) && target.contains(digit);
+	}
+
+	private boolean contains(Digit digit) {
+		return number.contains(digit);
+	}
+
+	private boolean equalsAt(int index, Digit digit) {
+		return number.get(index).equals(digit);
 	}
 }
