5cc7354318e1cea378fca32ebe1c3e4cb5502e86
KSKIM <kwangsoonkim@outlook.com>
2019-12-19 14:44:05 +0900

    feat: 볼 값을 가지는 Ball 추가

---
+++ src/main/java/com/woowacourse/baseball/domain/ballcount/Ball.java
@@ -0,0 +1,35 @@
+package com.woowacourse.baseball.domain.ballcount;
+
+import static com.woowacourse.baseball.domain.number.Number.*;
+
+/**
+ * @version 1.0.0
+ * @author KSKIM
+ * @since 2019-12-19
+ */
+public class Ball {
+	private static final String OUT_OF_RANGE_EXCEPTION = "볼의 갯수가 올바르지 않습니다";
+	private static final int MIN_BALL_COUNT = 0;
+
+	private final int ball;
+
+	public Ball(int ball) {
+		validateBall(ball);
+		this.ball = ball;
+	}
+
+	private void validateBall(int ball) {
+		if (ball < MIN_BALL_COUNT || ball > TOTAL_NUMBER_COUNT) {
+			throw new IllegalArgumentException(OUT_OF_RANGE_EXCEPTION);
+		}
+	}
+
+	public boolean exist() {
+		return ball > MIN_BALL_COUNT;
+	}
+
+	@Override
+	public String toString() {
+		return String.valueOf(ball);
+	}
+}
